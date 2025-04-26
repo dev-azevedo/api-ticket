796675c8146009ece8c285fcaa1cadf0f5765264
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-04-26 15:30:39 -0300

    feat: implementing validator data in ticker dto

--- src/DTO/ticket_dto.py
+++ src/DTO/ticket_dto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 from src.enum.ticket_status_enum import ETicketStatus
 
@@ -8,9 +8,38 @@ class TicketBaseDTO(BaseModel):
     description: str
     status: int
     
-    
+   
 class TicketCreateDTO(TicketBaseDTO):
-    pass
+    
+    @field_validator("status")
+    def validate_status(cls, value):
+        if value not in ETicketStatus:
+            raise ValueError("Status must be one of the following: 1(OPEN), 2(CLOSED), 3(PENDING)")
+
+        return value
+    
+    @field_validator("name_user")
+    def validate_name_use(cls, value):
+        if len(value) < 3:
+            raise ValueError("Name, Title and Description must be at least 3 characters")
+        
+        return value
+    
+    @field_validator("title")
+    def validate_title(cls, value):
+        if len(value) < 3:
+            raise ValueError("Name, Title and Description must be at least 3 characters")
+        
+        return value
+    
+    @field_validator("description")
+    def validate_description(cls, value):
+        if len(value) < 3:
+            raise ValueError("Name, Title and Description must be at least 3 characters")
+        
+        return value
+    
+    
 
 class TicketUpdateDTO(TicketBaseDTO):
     id: int
